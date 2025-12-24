8d8c064ded615fb10029a511f94a5986c993291d
Itebimien Momodu <fluxflareltd@gmail.com>
2025-12-24 15:24:26 +0100

    Create schema.ts

---
+++ apps/web/src/lib/config/schema.ts
@@ -0,0 +1,33 @@
+export interface GoogleSheetsConfig {
+  spreadsheetId: string;
+  sheets: {
+    properties: string;
+    apartmentTypes: string;
+    apartments: string;
+    tenants: string;
+    occupancies: string;
+    rents: string;
+    bills: string;
+    payments: string;
+    allocations: string;
+    documents: string;
+  };
+}
+
+export interface GoogleDriveConfig {
+  rootFolderId: string;
+  folders: {
+    receipts: string;
+    statements: string;
+    agreements: string;
+    exports: string;
+  };
+}
+
+export interface AppConfig {
+  backend: "google" | "postgres";
+  google?: {
+    sheets: GoogleSheetsConfig;
+    drive: GoogleDriveConfig;
+  };
+}
