c75d94de0ef2df7fa3a1628f7822224f1ca665f5
Itebimien Momodu <fluxflareltd@gmail.com>
2025-12-24 15:13:44 +0100

    Update google.ts

--- scripts/google-bootstrap/src/google.ts
+++ scripts/google-bootstrap/src/google.ts
@@ -1,1 +1,26 @@
+import { google } from "googleapis";
+import fs from "fs";
 
+export function getGoogleClients() {
+  const keyPath = process.env.GOOGLE_SERVICE_ACCOUNT_JSON;
+  if (!keyPath) {
+    throw new Error("GOOGLE_SERVICE_ACCOUNT_JSON is not set");
+  }
+
+  const credentials = JSON.parse(fs.readFileSync(keyPath, "utf8"));
+
+  const auth = new google.auth.JWT(
+    credentials.client_email,
+    undefined,
+    credentials.private_key,
+    [
+      "https://www.googleapis.com/auth/drive",
+      "https://www.googleapis.com/auth/spreadsheets"
+    ]
+  );
+
+  return {
+    drive: google.drive({ version: "v3", auth }),
+    sheets: google.sheets({ version: "v4", auth })
+  };
+}
