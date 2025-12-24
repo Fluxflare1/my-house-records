5d9af57c3259236427a2087c4ce7613c565431ea
Itebimien Momodu <fluxflareltd@gmail.com>
2025-12-24 15:42:01 +0100

    Create apartment-type.ts

---
+++ apps/web/src/lib/types/apartment-type.ts
@@ -0,0 +1,8 @@
+export interface ApartmentType {
+  apartment_type_id: string;
+  name: string;
+  yearly_rent_amount: number;
+  monthly_charge_amount: number;
+  active: boolean;
+  created_at: string;
+}
